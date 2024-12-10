d34c8da23771f6475f279f7c8d184ea21a920626
G1OsU <giosue.laurenzi@risikamente.it>
2024-12-10 13:29:15 +0100

    spostati file

--- src/main/java/com/mycompany/torneo_monacchia/applicazione.java
+++ src/main/java/com/mycompany/torneo_monacchia/applicazione.java
@@ -1,3 +1,5 @@
+package com.mycompany.torneo_monacchia;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -7,6 +9,6 @@
  *
  * @author B045_09
  */
-public class giocatore {
+public class applicazione {
     
 }
--- src/main/java/com/mycompany/torneo_monacchia/arbitro.java
+++ src/main/java/com/mycompany/torneo_monacchia/arbitro.java
@@ -1,3 +1,5 @@
+package com.mycompany.torneo_monacchia;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -7,6 +9,6 @@
  *
  * @author B045_09
  */
-public class squadra {
+public class arbitro {
     
 }
